df7d5e383bfd69bab06d5229cda178550fef4216
lexiyvv <76874548+lexiyvv@users.noreply.github.com>
2022-08-27 22:33:32 +0800

    Update main.js
    Solved the problem of text viewing in /contests, etc.

--- main.js
+++ main.js
@@ -3,71 +3,77 @@
 // @namespace    http://www.nfls.com.cn:20035/article/1197
 // @version      0.3.2
 // @description  Use NFLSOJ More Easily
-// @author       lexiyvv & ppip & GlaceonVGC & ACrazySteve
+// @author       lexiyvv & ppip
 // @match      *://www.nfls.com.cn:20035/*
 // @match      *://192.168.188.77/*
 // @grant        GM_setClipboard
-// @icon         https://raw.githubusercontent.com/NFLSCode/nflsoj-helper/master/icon.png
-// @icon64       https://raw.githubusercontent.com/NFLSCode/nflsoj-helper/master/icon.png
 // ==/UserScript==
 
 function getColor(request) {
-    let fir = request.match(/(?<=##)#[0-9a-fA-F]{6}/),
-        res = request.match(/(?<=(?<!#)#)#[0-9a-fA-F]{6}/);
+    var fir = request.match(/(?<=##)#[0-9a-fA-F]{6}/), res = request.match(/(?<=(?<!#)#)#[0-9a-fA-F]{6}/);
     res = res ? res[0] : "black";
     return [fir ? fir[0] : res, res];
 }
-function genColorHTML(t, data, name, color) {
-    return `<${t} ${data}><span style="color:${color[0]}">${name[0]}</span><span style="color:${color[1]};">${name.slice(1)}</span></${t}>`;
+function genColorHTML(t, data, name) {
+    return function(color) {
+        return `<${t} ${data} style="color:${color[0]};">${name[0]}</${t}><${t} ${data} style="color:${color[1]};">${name.slice(1)}</${t}>`;
+    };
 }
 function getUserIcon(request) {
-    let icon = request.match(/##\{(\w+ icon)\}/);
+    var icon = request.match(/##\{(\w+ icon)\}/);
     return icon ? `<i class="${icon[1]}"></i>` : null;
 }
-let tourist = {"20200131": ["black", "red"], "sszcdjr": ["black", "red"]}, domain = window.location.pathname;
-if (!localStorage.getItem("bgurl")) {
-    localStorage.setItem("bgurl", "https://cdn.luogu.com.cn/upload/image_hosting/t9gah1kv.png");
-}
-document.body.style.backgroundImage=`url(${localStorage.getItem("bgurl")})`;
-document.body.style.backgroundSize="cover";
-if (!localStorage.getItem("fgopacity")) {
-    localStorage.setItem("fgopacity", "0.8");
-}
-document.body.style.opacity = localStorage.getItem("fgopacity");
-Array.from(document.getElementsByClassName("hl-source")).forEach(function(value, index, array) {
-    value = value.parentNode.parentNode.parentNode;
-    value.innerHTML = "<button style='width:90px;height:28px;border:1px solid black;border-radius:4px;'>Copy</button>" + value.innerHTML;
-    value.childNodes[0].addEventListener("click", function() {
-        GM_setClipboard(value.lastChild.textContent, "text");
-        value.childNodes[0].textContent = "Copied!";
-        setTimeout(function() {
-            value.childNodes[0].textContent = "Copy";
-        }, 1000);
-    })
-});
-if (domain == "/") {
-    for(var i = 1; i < 40; i += 2) {
-        let td = document.getElementsByClassName("ui very basic center aligned table")[0].tBodies[0].childNodes[i], name = td.childNodes[3].innerText;
-        td.childNodes[3].innerHTML = genColorHTML(
-            "a", `href=${td.childNodes[3].childNodes[0].getAttribute("href")}`, name,
-            tourist.hasOwnProperty(name) ? tourist[name] : getColor(td.childNodes[9].textContent));
+(function() {
+    'use strict';
+    var imageurl="https://cdn.luogu.com.cn/upload/image_hosting/t9gah1kv.png";
+    document.body.style.backgroundImage="url("+imageurl+")";
+    document.body.style.backgroundRepeat="no-repeat";
+    document.body.style.backgroundSize="cover";
+ //   alert(document.getElementsByClassName("blurring dimmable image")[0].childNodes);
+ 	//these five lines for background
+    var domain = window.location.pathname;
+    //background fixs
+    if(/contests/.test(domain)){document.getElementsByClassName("padding")[0].childNodes[1].style.backgroundColor="rgba(255,255,255,0.8)";document.getElementsByClassName("padding")[0].childNodes[1].style.border="thin solid rgba(200,200,200,0.5)"}//fk IE
+    else if(/submissions/.test(domain)){document.getElementsByClassName("padding")[0].childNodes[3].style.backgroundColor="rgba(255,255,255,0.8)";document.getElementsByClassName("padding")[0].childNodes[3].style.border="thin solid rgba(200,200,200,0.5)"}//fk IE
+    else if(/discussion/.test(domain)){document.getElementsByClassName("padding")[0].childNodes[3].style.backgroundColor="rgba(255,255,255,0.8)";document.getElementsByClassName("padding")[0].childNodes[3].style.border="thin solid rgba(200,200,200,0.5)"}//fk IE
+    else if((/ranklist/.test(domain))&&(/contest/.test(domain))){document.getElementsByClassName("padding")[0].childNodes[3].style.backgroundColor="rgba(255,255,255,0.8)";document.getElementsByClassName("padding")[0].childNodes[3].style.border="thin solid rgba(200,200,200,0.5)"}//fk IE
+    else{document.body.style.opacity="0.8";}
+    if (domain == "/") {
+        for(var i = 1; i < 40; i += 2) {
+            var td = document.getElementsByClassName("ui very basic center aligned table")[0].tBodies[0].childNodes[i];
+            var name = td.childNodes[3].innerText,
+                sign = td.childNodes[9].textContent,
+                result = genColorHTML("a", `href=${td.childNodes[3].childNodes[0].getAttribute("href")}`, name);
+            let tourist = ["20200131", "sszcdjr", "NFLSOJ"];
+            td.childNodes[3].innerHTML = tourist.includes(name) ? result(["black", "red"]) : result(getColor(sign));
+        }
+        var board = document.getElementsByClassName("ui very basic table")[0];
+        board.innerHTML += "<text>NFLSOJ helper 公告</text><hr>"
+                         + "<a href='/article/1197'>NFLSOJ helper 发布帖</a><hr>"
+                         + "<text style='border:1px solid black;border-radius:4px;'>延长登录时间</text>";
+        board.childNodes[9].addEventListener("click", function() {
+            document.cookie = `${document.cookie.match(/(^| )(login=[^;]*)(;|$)/)[2]};expires=Wed, 04 Aug 2077 01:00:00 GMT`;
+            alert("Success");
+        });
+    } else if (/\/user\/[0-9]+(\/(?!edit)|$)/.test(domain)) {
+        var imageurl2="https://cdn-userpic.codeforces.com/1199019/title/4dc19a4ef4c17004.jpg";
+        document.getElementsByClassName("blurring dimmable image")[0].childNodes[3].src=imageurl2;
+ 		//these three lines for pictures
+        var mainpage = document.getElementsByClassName("ui bottom attached segment"),
+            nameColor = genColorHTML("nobr", "", mainpage[0].innerHTML)(getColor(mainpage[3].innerHTML)),
+            backup = document.getElementsByClassName("icon")[14].outerHTML, customIcon = getUserIcon(mainpage[3].innerHTML);
+        backup = customIcon ? customIcon : /(man|woman) icon/.test(backup) ? backup : "";
+        mainpage[0].innerHTML = nameColor;
+        document.getElementsByClassName("header")[1].innerHTML = nameColor + " " + backup;
+    } else if (/submission(?=[^s])/.test(domain)) {
+        var button = document.getElementsByClassName("ui very basic table")[0];
+        button.innerHTML += "<button style='width:90px;height:28px;border:1px solid black;border-radius:4px;'>Copy</button>";
+        button = button.childNodes[3];
+        button.addEventListener("click", function() {
+            var s = document.getElementsByClassName("hl-c++")[0].textContent;
+            GM_setClipboard(s, "text");
+            button.textContent = "Copied!";
+            setTimeout(function(){button.textContent = "Copy"}, 1000);
+        });
     }
-    let board = document.getElementsByClassName("ui very basic table")[0];
-    board.innerHTML += `<text>NFLSOJ helper 公告</text><hr>
-                        <a href='/article/1197'>NFLSOJ helper 发布帖</a><hr>
-                        <text style='border:1px solid black;border-radius:4px;'>延长登录时间</text>`;
-    board.childNodes[9].addEventListener("click", function() {
-        document.cookie = `${document.cookie.match(/(^| )(login=[^;]*)(;|$)/)[2]};expires=Wed, 04 Aug 2077 01:00:00 GMT`;
-        alert("Success");
-    });
-} else if (/\/user\/[0-9]+(\/(?!edit)|$)/.test(domain)) {
-    var imageurl2="https://cdn-userpic.codeforces.com/1199019/title/4dc19a4ef4c17004.jpg";
-    document.getElementsByClassName("blurring dimmable image")[0].childNodes[3].src=imageurl2;
-    let mainpage = document.getElementsByClassName("ui bottom attached segment"),
-        nameColor = genColorHTML("nobr", "", mainpage[0].innerHTML, getColor(mainpage[3].innerHTML)),
-        backup = document.getElementsByClassName("icon")[14].outerHTML,
-        customIcon = getUserIcon(mainpage[3].innerHTML);
-    backup = customIcon ? customIcon : /(man|woman) icon/.test(backup) ? backup : "";
-    mainpage[0].innerHTML = nameColor;
-    document.getElementsByClassName("header")[1].innerHTML = nameColor + " " + backup;
-}
+})();
