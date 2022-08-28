2b66fc3345f9adbd95e1c2b945f54b67a44324e0
GlaceonVG <GlaceonVGC@163.com>
2022-08-28 19:55:01 +0800

    Supported copying sample

--- main.js
+++ main.js
@@ -45,14 +45,14 @@ if (!localStorage.getItem("fgopacity")) {
     localStorage.setItem("fgopacity", "0.8");
 }
 document.body.style.opacity = localStorage.getItem("fgopacity");
-function copy(value) {
-    return function() {
-        GM_setClipboard(value.lastChild.textContent, "text"); // eslint-disable-line no-undef
-        value.firstChild.textContent = "Copied!";
+function add_copy(a, b) {
+    a.addEventListener("click", function() {
+        GM_setClipboard(b.textContent, "text");
+        a.textContent = "Copied!";
         setTimeout(function() {
-            value.firstChild.textContent = "Copy";
+            a.textContent = "Copy";
         }, 1000);
-    }
+    })
 }
 if (/^\/submission\/\d+$/.test(domain)) {
     let value = document.getElementsByClassName("ui existing segment")[0];
@@ -60,13 +60,15 @@ if (/^\/submission\/\d+$/.test(domain)) {
     value.firstChild.style.position = "unset";
     let position = value.innerHTML.search(/<\/a>/) + 4;
     value.innerHTML = `<span style="position:absolute;top:0px;right:-4px;"><div class="ui button" style="position:relative;left:4px;border-right: 1px solid rgba(0,0,0,0.6);border-radius: 0 0 0 .28571429rem;">Copy</div>${value.innerHTML.slice(0, position)}</span>${value.innerHTML.slice(position)}`;
-    value.firstChild.firstChild.addEventListener("click", copy(value));
+    add_copy(value.firstChild.firstChild, value.lastChild);
 } else {
-    Array.from(document.getElementsByClassName("hl-source")).forEach(function(value) {
-        value = value.parentNode.parentNode.parentNode;
-        value.innerHTML = `<div class="ui button" style="position:absolute;top:0px;right:-4px;border-top-left-radius:0;border-bottom-right-radius:0;">Copy</div>` + value.innerHTML;
-        value.firstChild.addEventListener("click", copy(value));
-    });
+    for (let i = 0, e; e = document.getElementsByClassName("ui existing segment"), i < e.length; i++) {
+        if (/\/problem\//.test(domain)) {
+            e[i].parentNode.style.width = "50%";
+        }
+        e[i].innerHTML = `<div class="ui button" style="position:absolute;top:0px;right:-4px;border-top-left-radius:0;border-bottom-right-radius:0;">Copy</div>` + e[i].innerHTML;
+        add_copy(e[i].firstChild, e[i].childNodes[e[i].childNodes.length / 2]);
+    }
 }
 if (domain == "/") {
     for(var i = 1; i < 40; i += 2) {
